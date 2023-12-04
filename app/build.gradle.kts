b5cc1e3d55c7f86f538e4c7ecb8b4f37f85655d9
Чернышов Артём Николаевич <pr120chernyshov@oat.local>
2023-12-04 16:40:38 +0600

    4.12.23

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    kotlin("plugin.serialization") version "1.9.0"
 }
 
 android {
@@ -37,12 +38,15 @@ android {
 
 dependencies {
 
+    implementation("androidx.core:core-ktx:1.9.0")
     implementation(platform("io.github.jan-tennert.supabase:bom:1.2.0"))
     implementation("io.github.jan-tennert.supabase:postgrest-kt")
-    implementation("io.github.jan-tennert.supabase:realtime-kt")
+    implementation("io.github.jan-tennert.supabase:gotrue-kt")
+    implementation("io.ktor:ktor-client-cio:2.3.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 
 
-    implementation("androidx.core:core-ktx:1.12.0")
+
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
